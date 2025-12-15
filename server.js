53604ab4aac985ea0da9cf285e8113df463dccb3
BARSOUM johnny (PEpsi) <pepsijohnny22@gmail.com>
2025-12-15 23:33:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,3 +34,26 @@ io.emit('state', users);
 server.listen(3000, () => {
 console.log('SWAEB running on http://localhost:3000');
 });
+const rooms = {};
+
+
+io.on('connection', socket => {
+socket.on('join-room', roomId => {
+socket.join(roomId);
+if (!rooms[roomId]) rooms[roomId] = {};
+rooms[roomId][socket.id] = { x: 100, y: 100 };
+io.to(roomId).emit('state', rooms[roomId]);
+
+
+socket.on('move', pos => {
+rooms[roomId][socket.id] = pos;
+io.to(roomId).emit('state', rooms[roomId]);
+});
+
+
+socket.on('disconnect', () => {
+delete rooms[roomId][socket.id];
+io.to(roomId).emit('state', rooms[roomId]);
+});
+});
+});
